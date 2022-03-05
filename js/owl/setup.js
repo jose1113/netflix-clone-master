6b1745ec6ccf65aee8ffc822ff26761f659319a8
jose1113 <joseigor113@gmail.com>
2022-03-05 15:45:06 -0300

    fixed arrows

--- js/owl/setup.js
+++ js/owl/setup.js
@@ -1,11 +1,32 @@
 var dots = $('.owl-dot')
 var owl = $('.owl-carousel')
-var owl2 = $("#first-carousel")
+var owl1 = $("#first-carousel")
+var owl2 = $("#second-carousel")
+var owl3 = $("#third-carousel")
 
 owl.owlCarousel({
-    loop:false,
-    margin:7,
-    nav:false,
+    loop: false,
+    margin: 7,
+    nav: false,
+    dots: false,
+    stagePadding: 50,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
+
+owl1.owlCarousel({
+    loop: false,
+    margin: 7,
+    nav: false,
     dots: false,
     stagePadding: 50,
     responsive:{
@@ -22,9 +43,28 @@ owl.owlCarousel({
 })
 
 owl2.owlCarousel({
-    loop:false,
-    margin:7,
-    nav:false,
+    loop: false,
+    margin: 7,
+    nav: false,
+    dots: false,
+    stagePadding: 50,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
+
+owl3.owlCarousel({
+    loop: false,
+    margin: 7,
+    nav: false,
     dots: false,
     stagePadding: 50,
     responsive:{
@@ -41,11 +81,24 @@ owl2.owlCarousel({
 })
 
 $("#botao_anterior").on('click', function(){
-    owl2.trigger('prev.owl.carousel')
+    owl1.trigger('prev.owl.carousel')
+})
+$("#botao_proximo").on('click', function(){
+    owl1.trigger('next.owl.carousel')
 })
 
-$("#botao_proximo").on('click', function(){
+$("#botao_anterior1").on('click', function(){
+    owl2.trigger('prev.owl.carousel')
+})
+$("#botao_proximo1").on('click', function(){
     owl2.trigger('next.owl.carousel')
+})
+
+$("#botao_anterior2").on('click', function(){
+    owl3.trigger('prev.owl.carousel')
+})
+$("#botao_proximo2").on('click', function(){
+    owl3.trigger('next.owl.carousel')
 })
 
 window.onload = function () {
@@ -62,3 +115,16 @@ function emcima1() {
 function sair1() {
     document.getElementById("img1").src = "./img/mini1.jpg"
 }
+
+$(document).ready(function() {$(".btn").css('visibility','hidden')})
+$(document).ready(function() {$(".btn1").css('visibility','hidden')})
+$(document).ready(function() {$(".btn2").css('visibility','hidden')})
+
+$(".carrosel-filmes1").mouseover(function() { $(".btn").css('visibility','visible'); })
+$(".carrosel-filmes1").mouseout(function() { $(".btn").css('visibility','hidden'); })
+
+$(".carrosel-filmes2").mouseover(function() { $(".btn1").css('visibility','visible'); })
+$(".carrosel-filmes2").mouseout(function() { $(".btn1").css('visibility','hidden'); })
+
+$(".carrosel-filmes3").mouseover(function() { $(".btn2").css('visibility','visible'); })
+$(".carrosel-filmes3").mouseout(function() { $(".btn2").css('visibility','hidden'); })
